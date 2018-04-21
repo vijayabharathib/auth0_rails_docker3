752e7f8e8033e1df4c08302aea67ac9a53488fbd
vijayabharathi <vijayabharathib@users.noreply.github.com>
2018-04-21 11:15:16 +0530

    session store in dev only

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,2 +1,4 @@
 #Rails.application.config.session_store :cache_store
-Rails.application.config.session_store :cookie_store, key: '_app_session', domain: :all 
+if Rails.env.development?
+    Rails.application.config.session_store :cookie_store, key: '_app_session', domain: :all 
+end
