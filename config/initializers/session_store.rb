c6a0a805867911b951841395984225bf80ed5be5
vijayabharathi <vijayabharathib@users.noreply.github.com>
2018-04-21 11:25:00 +0530

    remove session store config to avoid csrf detected

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,4 +1,2 @@
 #Rails.application.config.session_store :cache_store
-if Rails.env.development?
-    Rails.application.config.session_store :cookie_store, key: '_app_session', domain: :all 
-end
+#Rails.application.config.session_store :cookie_store, key: '_app_session', domain: :all 
